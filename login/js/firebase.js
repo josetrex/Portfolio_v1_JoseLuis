bbad1974d00233dd6b19a0c20479d3efa68aa72c
Jose Luis Astudillo Idrovo <josetrex34@gmail.com>
2024-10-08 22:25:58 -0700

    hola

---
+++ login/js/firebase.js
@@ -0,0 +1,25 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBoRuMQ3MObHG6rsc6SanVOh3VHsx3b6Tg",
+  authDomain: "portfoliov1joseluis-afbf5.firebaseapp.com",
+  projectId: "portfoliov1joseluis-afbf5",
+  storageBucket: "portfoliov1joseluis-afbf5.appspot.com",
+  messagingSenderId: "1012348207973",
+  appId: "1:1012348207973:web:98b7a09cfbde394597d78c",
+  measurementId: "G-7R7FRHNXXZ"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
